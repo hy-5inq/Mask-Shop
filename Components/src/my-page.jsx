ab3f1539271eafd6beb3dd8fa4e9bc3118262f54
taeuk.kang <gmdkrl@hanyang.ac.kr>
2019-06-11 15:13:59 +0900

    test

--- Components/src/my-page.jsx
+++ Components/src/my-page.jsx
@@ -8,123 +8,123 @@ import '../stylesheets/my-page.css'
 
 class MyPage extends React.Component {
 
-    handleFold(){
+	handleFold(){
 
-        let SnapCard = document.body.querySelector(`#MY_PAGE`)
-        SnapCard.classList.add(`--Fold-Off`)
+		const SnapCard = document.body.querySelector(`#MY_PAGE`)
+		SnapCard.classList.add(`--Fold-Off`)
 
-    }
+	}
 
-    render(){
+	render(){
 
-        return (
+		return (
 
-            <div id="MY_PAGE" className="SnapCard-Container --Fold-Off">
+			<div id="MY_PAGE" className="SnapCard-Container --Fold-Off">
 
-                <div className="SnapCard-Container__Header --Bg-OrderCart">
+				<div className="SnapCard-Container__Header --Bg-OrderCart">
 
-                    <div className="Header__Item">
-                        <MdPerson className="Header__Item__Icon" />
-                    </div>
+					<div className="Header__Item">
+						<MdPerson className="Header__Item__Icon" />
+					</div>
 
-                    <div className="Header__Item">
-                        <h3 className="Header__Item__Text">마이 페이지</h3>
-                    </div>
+					<div className="Header__Item">
+						<h3 className="Header__Item__Text">마이 페이지</h3>
+					</div>
 
-                    <div className="Header__Item">
-                        <MdKeyboardArrowLeft onClick={this.handleFold} className="Header__Item__Icon --Fold" />
-                    </div>
-                </div>
+					<div className="Header__Item">
+						<MdKeyboardArrowLeft onClick={this.handleFold} className="Header__Item__Icon --Fold" />
+					</div>
+				</div>
 
-                <div className="MyPage-Grid-Container">
-                    <div className="MyPage-Grid-Container__Item">
+				<div className="MyPage-Grid-Container">
+					<div className="MyPage-Grid-Container__Item">
                        
-                        <div className="MyPage-Grid-Container__Item__User-Box">
+						<div className="MyPage-Grid-Container__Item__User-Box">
 
-                            <div className="MyPage-Grid-Container__Item__User-Box__Img-Box">
-                                <img src="" alt="회원 이미지"/>
-                            </div>
-                            <div className="MyPage-Grid-Container__Item__User-Box__Text-Box">
+							<div className="MyPage-Grid-Container__Item__User-Box__Img-Box">
+								<img src="" alt="회원 이미지"/>
+							</div>
+							<div className="MyPage-Grid-Container__Item__User-Box__Text-Box">
 
-                                <div>
-                                    <span className="MyPage-Grid-Container__Item__User-Box__Text-Box__Text">함성준</span>
-                                </div>
+								<div>
+									<span className="MyPage-Grid-Container__Item__User-Box__Text-Box__Text">함성준</span>
+								</div>
                                 
-                            </div>
+							</div>
 
-                        </div>
+						</div>
 
-                        <div className="MyPage-Grid-Container__Item__Info-Box">
-                            <div className="MyPage-Grid-Container__Item__Info-Box__Item">
+						<div className="MyPage-Grid-Container__Item__Info-Box">
+							<div className="MyPage-Grid-Container__Item__Info-Box__Item">
 
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">나의 레벨</span>
-                                </div>
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text --Color-Brown">Bronze</span>
-                                </div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">나의 레벨</span>
+								</div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text --Color-Brown">Bronze</span>
+								</div>
                                 
-                            </div>
-                            <div className="MyPage-Grid-Container__Item__Info-Box__Item">
+							</div>
+							<div className="MyPage-Grid-Container__Item__Info-Box__Item">
 
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">나의 댓글</span>
-                                </div>
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text">0</span>
-                                </div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">나의 댓글</span>
+								</div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text">0</span>
+								</div>
 
-                            </div>
-                            <div className="MyPage-Grid-Container__Item__Info-Box__Item">
+							</div>
+							<div className="MyPage-Grid-Container__Item__Info-Box__Item">
 
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">나의 리뷰</span>
-                                </div>
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text">0</span>
-                                </div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">나의 리뷰</span>
+								</div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text">0</span>
+								</div>
 
-                            </div>
-                            <div className="MyPage-Grid-Container__Item__Info-Box__Item">
+							</div>
+							<div className="MyPage-Grid-Container__Item__Info-Box__Item">
 
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">마일리지</span>
-                                </div>
-                                <div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
-                                    <span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text">76,000</span>
-                                </div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Header">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Header__Text">마일리지</span>
+								</div>
+								<div className="MyPage-Grid-Container__Item__Info-Box__Item__Content">
+									<span className="MyPage-Grid-Container__Item__Info-Box__Item__Content__Text">76,000</span>
+								</div>
 
-                            </div>
-                        </div>
+							</div>
+						</div>
 
-                        <div className="MyPage-Grid-Container__Item__Nav-box">
+						<div className="MyPage-Grid-Container__Item__Nav-box">
 
-                            <FaHistory className="MyPage-Grid-Container__Item__Nav-box__Icon"/>
-                            <span className="MyPage-Grid-Container__Item__Nav-box__Text">주문 조회</span>
+							<FaHistory className="MyPage-Grid-Container__Item__Nav-box__Icon"/>
+							<span className="MyPage-Grid-Container__Item__Nav-box__Text">주문 조회</span>
 
-                        </div>
+						</div>
 
-                        <div className="MyPage-Grid-Container__Item__Nav-box">
+						<div className="MyPage-Grid-Container__Item__Nav-box">
 
-                            <FaTruckMoving className="MyPage-Grid-Container__Item__Nav-box__Icon"/>
-                            <span className="MyPage-Grid-Container__Item__Nav-box__Text">배송 조회</span>
+							<FaTruckMoving className="MyPage-Grid-Container__Item__Nav-box__Icon"/>
+							<span className="MyPage-Grid-Container__Item__Nav-box__Text">배송 조회</span>
 
-                        </div>
-                        <div className="MyPage-Grid-Container__Item__Nav-box">
+						</div>
+						<div className="MyPage-Grid-Container__Item__Nav-box">
 
-                            <FaUserCog className="MyPage-Grid-Container__Item__Nav-box__Icon"/>
-                            <span className="MyPage-Grid-Container__Item__Nav-box__Text">회원정보 수정</span>
+							<FaUserCog className="MyPage-Grid-Container__Item__Nav-box__Icon"/>
+							<span className="MyPage-Grid-Container__Item__Nav-box__Text">회원정보 수정</span>
 
-                        </div>
-                    </div>
+						</div>
+					</div>
                    
-                </div>
+				</div>
 
-            </div>
+			</div>
 
-        )
+		)
 
-    }
+	}
 
 
 }
--- Components/src/order-cart.jsx
+++ Components/src/order-cart.jsx
@@ -8,109 +8,109 @@ import '../stylesheets/order-cart.css'
 
 class OrderCart extends React.Component {
 
-    handleFold(){
+	handleFold(){
 
-        let SnapCard = document.body.querySelector(`#ORDER_CART`)
-        SnapCard.classList.add(`--Fold-Off`)
+		const SnapCard = document.body.querySelector(`#ORDER_CART`)
+		SnapCard.classList.add(`--Fold-Off`)
 
-    }
+	}
 
-    render(){
+	render(){
 
-        return (
+		return (
 
-            <div id="ORDER_CART" className="SnapCard-Container --Fold-Off">
+			<div id="ORDER_CART" className="SnapCard-Container --Fold-Off">
 
-                <div className="SnapCard-Container__Header --Bg-OrderCart">
+				<div className="SnapCard-Container__Header --Bg-OrderCart">
 
-                    <div className="Header__Item">
-                        <MdShoppingCart className="Header__Item__Icon" />
-                    </div>
+					<div className="Header__Item">
+						<MdShoppingCart className="Header__Item__Icon" />
+					</div>
 
-                    <div className="Header__Item">
-                        <h3 className="Header__Item__Text">장바구니</h3>
-                    </div>
+					<div className="Header__Item">
+						<h3 className="Header__Item__Text">장바구니</h3>
+					</div>
 
-                    <div className="Header__Item">
-                        <MdKeyboardArrowLeft onClick={this.handleFold} className="Header__Item__Icon --Fold" />
-                    </div>
-                </div>
+					<div className="Header__Item">
+						<MdKeyboardArrowLeft onClick={this.handleFold} className="Header__Item__Icon --Fold" />
+					</div>
+				</div>
 
-                <div className="OrderCart-Container">
+				<div className="OrderCart-Container">
                 
-                    <div className="OrderCart__Header">
-                        <div className="OrderCart__Header__Item">
-                            <span className="OrderCart__Header__Item__Text">선택</span>
-                        </div>
-                        <div className="OrderCart__Header__Item">
-                            <span className="OrderCart__Header__Item__Text">제품명</span>
-                        </div>
-                        <div className="OrderCart__Header__Item">
-                            <span className="OrderCart__Header__Item__Text">수량</span>
-                        </div>
-                        <div className="OrderCart__Header__Item">
-                            <span className="OrderCart__Header__Item__Text">가격</span>
-                        </div>
-                        <div className="OrderCart__Header__Item">
-                            <span className="OrderCart__Header__Item__Text">사이즈</span>
-                        </div>
-                        <div className="OrderCart__Header__Item">
-                            <span className="OrderCart__Header__Item__Text">정기배송</span>
-                        </div>
-                    </div>
+					<div className="OrderCart__Header">
+						<div className="OrderCart__Header__Item">
+							<span className="OrderCart__Header__Item__Text">선택</span>
+						</div>
+						<div className="OrderCart__Header__Item">
+							<span className="OrderCart__Header__Item__Text">제품명</span>
+						</div>
+						<div className="OrderCart__Header__Item">
+							<span className="OrderCart__Header__Item__Text">수량</span>
+						</div>
+						<div className="OrderCart__Header__Item">
+							<span className="OrderCart__Header__Item__Text">가격</span>
+						</div>
+						<div className="OrderCart__Header__Item">
+							<span className="OrderCart__Header__Item__Text">사이즈</span>
+						</div>
+						<div className="OrderCart__Header__Item">
+							<span className="OrderCart__Header__Item__Text">정기배송</span>
+						</div>
+					</div>
 
-                    <div className="OrderCart__Element">
-                        <div className="OrderCart__Element__Item">
-                            <input type="checkbox"/>
-                        </div>
-                        <div className="OrderCart__Element__Item">
-                            <span className="OrderCart__Element__Item__Text">마스크 MK-101</span>
-                        </div>
-                        <div className="OrderCart__Element__Item">
-                            <input className="OrderCart__Element__Item__Input-Number" type="Number"/>
-                        </div>
-                        <div className="OrderCart__Element__Item">
-                            <span className="OrderCart__Element__Item__Text">100,000</span>
-                        </div>
-                        <div className="OrderCart__Element__Item">
-                            <span className="OrderCart__Element__Item__Text">M</span>
-                        </div>
-                        <div className="OrderCart__Element__Item">
-                            <span className="OrderCart__Element__Item__Text">O</span>
-                        </div>
-                    </div>
+					<div className="OrderCart__Element">
+						<div className="OrderCart__Element__Item">
+							<input type="checkbox"/>
+						</div>
+						<div className="OrderCart__Element__Item">
+							<span className="OrderCart__Element__Item__Text">마스크 MK-101</span>
+						</div>
+						<div className="OrderCart__Element__Item">
+							<input className="OrderCart__Element__Item__Input-Number" type="Number"/>
+						</div>
+						<div className="OrderCart__Element__Item">
+							<span className="OrderCart__Element__Item__Text">100,000</span>
+						</div>
+						<div className="OrderCart__Element__Item">
+							<span className="OrderCart__Element__Item__Text">M</span>
+						</div>
+						<div className="OrderCart__Element__Item">
+							<span className="OrderCart__Element__Item__Text">O</span>
+						</div>
+					</div>
 
-                </div>
+				</div>
 
-                <div className="OrderCart-Controller">
+				<div className="OrderCart-Controller">
 
-                    <div className="Controller__Item">
-                        <div className="Controller__Item__Btn" id="REMOVE_ITEM">
-                            <FaTrashAlt className="Controller__Item__Btn__Icon" />
-                            <span className="Controller__Item__Btn__Text">선택상품 삭제</span>
-                        </div>
-                        <div className="Controller__Item__Btn" id="GO_ORDER">
-                            <FaCreditCard className="Controller__Item__Btn__Icon" />
-                            <span className="Controller__Item__Btn__Text">주문하기</span>
-                        </div>
-                    </div>
+					<div className="Controller__Item">
+						<div className="Controller__Item__Btn" id="REMOVE_ITEM">
+							<FaTrashAlt className="Controller__Item__Btn__Icon" />
+							<span className="Controller__Item__Btn__Text">선택상품 삭제</span>
+						</div>
+						<div className="Controller__Item__Btn" id="GO_ORDER">
+							<FaCreditCard className="Controller__Item__Btn__Icon" />
+							<span className="Controller__Item__Btn__Text">주문하기</span>
+						</div>
+					</div>
 
-                    <div className="Controller__Item">
-                    </div>
+					<div className="Controller__Item">
+					</div>
 
-                    <div className="Controller__Item Credit-Box">
-                        <span className="Credit-Box__Text">총액</span>
-                        <span className="Credit-Box__Divider">:</span>
-                        <span className="Credit-Box__Text">{`76,000`}</span>
-                    </div>
+					<div className="Controller__Item Credit-Box">
+						<span className="Credit-Box__Text">총액</span>
+						<span className="Credit-Box__Divider">:</span>
+						<span className="Credit-Box__Text">{`76,000`}</span>
+					</div>
 
-                </div>
+				</div>
 
-            </div>
+			</div>
 
-        )
+		)
 
-    }
+	}
 
 
 }
